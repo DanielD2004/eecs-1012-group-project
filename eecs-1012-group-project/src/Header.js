54c83c8c5143cf1c88e1428ea981786a50874b46
Daniel <daniel@gdplace.com>
2022-10-24 13:46:44 -0400

    Added base files for a react app

---
+++ eecs-1012-group-project/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+  return (
+    <div id="header">
+        <div>hey</div>
+        <div id="title">Image Finder</div>
+        <div>hey</div>
+    </div>
+  )
+}
+
+export default Header
